78b8a0d0e911f1963a897390e6e4b283061d9e11
bhanotr <rbhanot999@gmail.com>
2026-04-22 02:10:00 -0400

    Switch EC2 instance type to c7i-flex.large (Free Tier eligible, 4 GB RAM)

--- cdk/falcon_stack/falcon_stack.py
+++ cdk/falcon_stack/falcon_stack.py
@@ -107,7 +107,7 @@ class FalconStack(Stack):
         instance = ec2.Instance(
             self,
             "FalconInstance",
-            instance_type=ec2.InstanceType("t3.medium"),
+            instance_type=ec2.InstanceType("c7i-flex.large"),
             machine_image=ami,
             vpc=vpc,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
